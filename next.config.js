b2b6919ad0c8f9e9cad4ad06a227ed7aac46b3bd
Thanik Bhongbhibhat <thanikb@pinterest.com>
2018-04-15 12:45:01 -0700

    Enable webpack bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+
 module.exports = {
+  // webpack function is executed twice, once for the server and once for the client
+  webpack(config, { isServer }/* options */) {
+    // only analyze client bundles
+    // `.next/dist/bundles` is for server bundles
+    if (!isServer && process.env.ANALYZE) {
+      config.plugins.push(
+        new BundleAnalyzerPlugin({
+          analyzerMode: "static",
+          reportFilename: "./bundles/client.html",
+          openAnalyzer: true
+        })
+      );
+    }
+    return config;
+  },
+
   exportPathMap() {
     return {
       "/": { page: "/" },
